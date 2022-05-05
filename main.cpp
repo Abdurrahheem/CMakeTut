9e17c8a3f58bf631d9e1bc1e8f530c19f9883728
Abdurrahheem <Shtanchaev1994@gmail.com>
2022-05-05 21:45:46 +0300

    added support for including libraries in standart way

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "Adder/adder.h"
+#include <adder.h>
 
 int main(){
     std::cout << "Hey, its me again, adding real numbers \n";
-    std::cout << "Sum of two " << add(72.1f, 72.8f) << "\n"; 
+    std::cout << "NEW Sum of two " << add(72.1f, 72.8f) << "\n"; 
     return 0;
 }
